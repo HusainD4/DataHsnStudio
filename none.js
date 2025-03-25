2245aceb0a9d2d848cc4505a8a433b8e44b71bde
Husain Mulyansyah <145633025+HusainD4@users.noreply.github.com>
2025-03-25 18:11:08 +0700

    update

---
+++ none.js
@@ -0,0 +1,9 @@
+function removeHtmlFromUrl() {
+    if (window.location.href.endsWith('.html')) {
+      const newUrl = window.location.href.replace('.html', '');
+      history.replaceState(null, '', newUrl);
+    }
+  }
+  
+  removeHtmlFromUrl(); 
+  
