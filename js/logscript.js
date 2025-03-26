28a7849361b4ce592bac8288df7c1d852dec934e
Husain Mulyansyah <145633025+HusainD4@users.noreply.github.com>
2025-03-26 13:46:40 +0700

    update 1.1

--- js/logscript.js
+++ js/logscript.js
@@ -8,7 +8,7 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     // Validasi username dan password
     if (username === "Husain" && password === "Admin123!") {
         // Arahkan ke halaman admin
-        window.location.href = "admin/admin.html";
+        window.location.href = "admin.html";
     } else {
         // Tampilkan pesan error jika login gagal
         errorMessage.textContent = "Username atau Password salah!";
